46b1c147d736b5de77c4679da4ce84cf8e4320dd
Digitalroot <Digitalroot@users.noreply.github.com>
2024-10-18 11:37:32 -0700

    Update Auth_phpBB.php

--- includes/Auth_phpBB.php
+++ includes/Auth_phpBB.php
@@ -29,7 +29,7 @@
  * @author C4K3
  * @author Joel Haasnoot
  * @author Casey Peel 
- * @copyright 2004-2021 Digitalroot Technologies
+ * @copyright 2004-2025 Digitalroot Technologies
  * @license http://www.gnu.org/copyleft/gpl.html
  * @link https://github.com/Digitalroot/MediaWiki_PHPBB_Auth
  * @link http://digitalroot.net/
